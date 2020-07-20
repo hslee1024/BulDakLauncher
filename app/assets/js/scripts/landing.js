6aae2b3975f0a71bbf46f4aaa8c84298e4db1952
Hyunseo Lee <leehyunseo7105@gmail.com>
2020-07-20 21:00:41 +0900

    Add Korean Language & Customizing for BD Online
    Thanks to Daniel Scalzi (https://github.com/dscalzi/)

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -220,14 +220,14 @@ const refreshServerStatus = async function(fade = false){
     loggerLanding.log('Refreshing Server Status')
     const serv = DistroManager.getDistribution().getServer(ConfigManager.getSelectedServer())
 
-    let pLabel = 'SERVER'
-    let pVal = 'OFFLINE'
+    let pLabel = '접속자'
+    let pVal = '오프라인'
 
     try {
         const serverURL = new URL('my://' + serv.getAddress())
         const servStat = await ServerStatus.getStatus(serverURL.hostname, serverURL.port)
         if(servStat.online){
-            pLabel = 'PLAYERS'
+            pLabel = '접속자'
             pVal = servStat.onlinePlayers + '/' + servStat.maxPlayers
         }
 
